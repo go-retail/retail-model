c8ef834b206b924afafee366ca06cc407eb5c2b1
Anand Rao <anand@honnu.com>
2020-09-04 13:41:34 -0700

    cleaned up address

--- pkg/model/address.go
+++ pkg/model/address.go
@@ -2,10 +2,10 @@ package model
 
 // Unexported struct (only accessible inside package `model`)
 type address struct {
-	house   string `json:"ProductName"`
-	street  string `json:"ProductName"`
-	city    string `json:"ProductName"`
-	state   string `json:"ProductName"`
-	country string `json:"ProductName"`
-	zipCode string `json:"zipcode"`
+	House   string `json:"ProductName"`
+	Street  string `json:"Street"`
+	City    string `json:"City"`
+	State   string `json:"State"`
+	Country string `json:"Country"`
+	ZipCode string `json:"zipcode"`
 }
--- pkg/model/product.go
+++ pkg/model/product.go
@@ -8,6 +8,6 @@ type Product struct {
 	Catagory      string    `json:"ProductCatagory"`
 	ProductUnit   string    `json:"ProductMeasurementUnit"`
 	PricePerUnit  float64   `json:"ProductDescription"`
-	PriceCurrency string    `json:"PriceCurrency"`
+	PriceCurrency string    `json:"Currency"`
 	UpdateDate    time.Time `json:"UpdateDate"`
 }
