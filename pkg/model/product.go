7d7f49cf8a58e93efdaf9b2372e6533dca25c1af
Anand Rao <anand@honnu.com>
2020-09-04 12:41:25 -0700

    Added Currency to the Product struct

--- pkg/model/product.go
+++ pkg/model/product.go
@@ -4,9 +4,10 @@ import "time"
 
 //Product .. A Struct to hold a Sale Transaction
 type Product struct {
-	Name         string    `json:"ProductName"`
-	Catagory     string    `json:"ProductCatagory"`
-	ProductUnit  string    `json:"ProductMeasurementUnit"`
-	PricePerUnit float64   `json:"ProductDescription"`
-	UpdateDate   time.Time `json:"txnDate"`
+	Name          string    `json:"ProductName"`
+	Catagory      string    `json:"ProductCatagory"`
+	ProductUnit   string    `json:"ProductMeasurementUnit"`
+	PricePerUnit  float64   `json:"ProductDescription"`
+	PriceCurrency float64   `json:"USD"`
+	UpdateDate    time.Time `json:"txnDate"`
 }
